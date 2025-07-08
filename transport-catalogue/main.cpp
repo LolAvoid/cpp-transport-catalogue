06715855733e5b6e78addced3ecc270e0a1f6c76
LolAvoid <geaben@yandex.ru>
2025-07-08 21:58:10 +0700

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -6,30 +6,12 @@
 int main() {
     transport_catalogue::TransportCatalogue catalogue;
     input_reader::InputReader input_reader;
-
-    int n;
-    std::cin >> n;
-    std::cin.ignore(); 
-    
-    for (int i = 0; i < n; ++i) {
-        std::string line;
-        std::getline(std::cin, line);
-        input_reader.ParseLine(line);
-    }
-    
-    input_reader.ApplyCommands(catalogue);       
-
-    int q;
-    std::cin >> q;
-    std::cin.ignore(); 
-
     stat_reader::StatReader stat_reader(catalogue);
 
-    for (int i = 0; i < q; ++i) {
-        std::string line;
-        std::getline(std::cin, line);
-        stat_reader.ProcessQuery(line);
-    }       
+    input_reader.ReadInput(std::cin);
+    input_reader.ApplyCommands(catalogue);
+
+    stat_reader.ProcessQueries(std::cin, std::cout);
 
     return 0;
 }
