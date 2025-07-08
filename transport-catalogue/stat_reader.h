29c52b551fd807a79d84057f9582475c39574309
LolAvoid <geaben@yandex.ru>
2025-07-08 19:59:36 +0700

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,18 @@
-// место для вашего кода
+#pragma once
+
+#include <string>
+#include "transport_catalogue.h"
+
+namespace stat_reader {
+
+class StatReader {
+public:
+    explicit StatReader(const transport_catalogue::TransportCatalogue& catalogue);
+    void ProcessQuery(const std::string& bus_name) const;
+    void PrintAllBuses() const;
+
+private:
+    const transport_catalogue::TransportCatalogue& catalogue_;
+};
+
+} 
