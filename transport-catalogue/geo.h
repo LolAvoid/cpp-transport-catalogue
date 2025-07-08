3b8e2418d9edf277ec538a9fd1bc79f0cfb5ee8a
LolAvoid <geaben@yandex.ru>
2025-07-08 19:56:39 +0700

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -1,25 +1,23 @@
 #pragma once
-
+#include <iostream>
 #include <cmath>
+
+namespace geo {
 
 struct Coordinates {
     double lat;
     double lng;
-    bool operator==(const Coordinates& other) const {
-        return lat == other.lat && lng == other.lng;
-    }
-    bool operator!=(const Coordinates& other) const {
-        return !(*this == other);
-    }
 };
 
-inline double ComputeDistance(Coordinates from, Coordinates to) {
+inline double ComputeDistance(const Coordinates& from, const Coordinates& to) {
     using namespace std;
-    if (from == to) {
+    if (from.lat == to.lat && from.lng == to.lng) {
         return 0;
     }
     static const double dr = 3.1415926535 / 180.;
     return acos(sin(from.lat * dr) * sin(to.lat * dr)
                 + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * 6371000;
+        * 6371000; 
 }
+
+} // namespace geo
