a0654349d91a5c3c2e3e1b576f0351c1b3ff3037
LolAvoid <geaben@yandex.ru>
2025-07-08 22:00:36 +0700

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,14 +1,12 @@
 #pragma once
 
-#include <memory>
 #include <string>
 #include <string_view>
+#include <vector>
 #include <unordered_map>
 #include <unordered_set>
-#include <deque>
-#include <vector>
-#include <functional> 
-#include "geo.h" 
+#include <set>
+#include "geo.h"
 
 namespace transport_catalogue {
 
@@ -19,32 +17,35 @@ struct Stop {
 
 struct Bus {
     std::string name;
-    std::vector<const Stop*> stops; 
+    std::vector<const Stop*> stops;
     bool is_round_trip;
 };
 
 struct BusInfo {
-    int stop_count;            
-    int unique_stop_count;     
-    double route_length;       
+    int stop_count = 0;
+    int unique_stop_count = 0;
+    double route_length = 0.0;
 };
 
 class TransportCatalogue {
 public:
-    void AddStop(const std::string_view name, const geo::Coordinates& coordinates);
-    void AddBus(const std::string_view name, const std::vector<const Stop*>& stops, bool is_round_trip);
+    void AddStop(std::string_view name, const geo::Coordinates& coordinates);
+    void AddBus(std::string_view name, const std::vector<const Stop*>& stops, bool is_round_trip);
+    
     const Bus* FindBus(std::string_view name) const;
     const Stop* FindStop(std::string_view name) const;
+    
     BusInfo GetBusInfo(std::string_view name) const;
-    const std::unordered_set<std::string_view>& GetBusesByStop(std::string_view stop_name) const;
+    const std::set<std::string_view>& GetBusesByStop(std::string_view stop_name) const;
 
 private:
-    std::deque<std::string> string_storage_; 
-    std::deque<Stop> stops_; 
-    std::deque<Bus> buses_;   
-    std::unordered_map<std::string_view, const Stop*> stops_map_; 
-    std::unordered_map<std::string_view, const Bus*> buses_map_; 
-    std::unordered_map<std::string_view, std::unordered_set<std::string_view>> stop_to_buses_map_; 
+    std::vector<std::string> string_storage_;
+    std::vector<Stop> stops_;
+    std::vector<Bus> buses_;
+    
+    std::unordered_map<std::string_view, Stop*> stops_map_;
+    std::unordered_map<std::string_view, Bus*> buses_map_;
+    std::unordered_map<std::string_view, std::set<std::string_view>> stop_to_buses_map_;
 };
 
 } 
