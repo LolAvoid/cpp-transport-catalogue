f3df161b2c37b3cd0b9e885faae7480db2f327a2
LolAvoid <geaben@yandex.ru>
2025-07-09 19:54:25 +0700

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,19 +1,18 @@
 #pragma once
 
+#include <string>
 #include "transport_catalogue.h"
-#include <iostream>
 
 namespace stat_reader {
 
 class StatReader {
 public:
     explicit StatReader(const transport_catalogue::TransportCatalogue& catalogue);
-    
-    void ProcessQueries(std::istream& input, std::ostream& output) const;
-    void ProcessQuery(const std::string& name, std::ostream& output) const;
+    void ProcessQuery(const std::string_view& bus_name) const;
+    void PrintAllBuses() const;
 
 private:
     const transport_catalogue::TransportCatalogue& catalogue_;
 };
 
-} 
+} // namespace stat_reader
