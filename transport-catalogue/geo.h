af05675879797979e231cf43831a1de63c64e36a
LolAvoid <geaben@yandex.ru>
2025-07-09 19:51:16 +0700

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <iostream>
 #include <cmath>
 
@@ -15,13 +14,10 @@ inline double ComputeDistance(const Coordinates& from, const Coordinates& to) {
     if (from.lat == to.lat && from.lng == to.lng) {
         return 0;
     }
-
     static const double dr = 3.1415926535 / 180.;
-    static const double EARTH_RADIUS = 6371000; 
-    
     return acos(sin(from.lat * dr) * sin(to.lat * dr)
                 + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * EARTH_RADIUS;
+        * 6371000; // Радиус Земли в метрах
 }
 
 } // namespace geo
