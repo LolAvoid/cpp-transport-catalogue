f2de316eaeb081201c6e6f23ce24984f06b7cb27
LolAvoid <geaben@yandex.ru>
2025-07-09 19:53:35 +0700

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include "transport_catalogue.h"
 #include "input_reader.h"
@@ -6,12 +7,29 @@
 int main() {
     transport_catalogue::TransportCatalogue catalogue;
     input_reader::InputReader input_reader;
+
+    int n;
+    std::cin >> n;
+    std::cin.ignore(); 
+    
+    for (int i = 0; i < n; ++i) {
+        std::string line;
+        std::getline(std::cin, line);
+        input_reader.ParseLine(line);
+    }
+    
+    input_reader.ApplyCommands(catalogue);       
+
+    int q;
+    std::cin >> q;
+    std::cin.ignore(); 
+
     stat_reader::StatReader stat_reader(catalogue);
 
-    input_reader.ReadInput(std::cin);
-    input_reader.ApplyCommands(catalogue);
-
-    stat_reader.ProcessQueries(std::cin, std::cout);
-
+    for (int i = 0; i < q; ++i) {
+        std::string line;
+        std::getline(std::cin, line);
+        stat_reader.ProcessQuery(line);
+    }       
     return 0;
 }
