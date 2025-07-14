01cc1aa2d767af757669b8b471f770eafed01d3c
LolAvoid <geaben@yandex.ru>
2025-07-14 16:59:10 +0700

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -2,11 +2,22 @@
 
 namespace geo {
 
+// Структура для хранения географических координат
 struct Coordinates {
-    double lat; // Широта
-    double lng; // Долгота
+    double lat;  // Широта в градусах
+    double lng;  // Долгота в градусах
+    
+    // Операторы сравнения для удобства тестирования
+    bool operator==(const Coordinates& other) const {
+        return lat == other.lat && lng == other.lng;
+    }
+    
+    bool operator!=(const Coordinates& other) const {
+        return !(*this == other);
+    }
 };
 
-double ComputeDistance(const Coordinates from, const Coordinates to);
+// Вычисляет расстояние между двумя точками на Земле (в метрах)
+double ComputeDistance(Coordinates from, Coordinates to);
 
 }  // namespace geo
