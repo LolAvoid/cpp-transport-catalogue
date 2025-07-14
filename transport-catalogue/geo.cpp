994c6a9cc48adf98bcf2c5c43eddddac244de513
LolAvoid <geaben@yandex.ru>
2025-07-14 17:22:10 +0700

    Update geo.cpp

--- transport-catalogue/geo.cpp
+++ transport-catalogue/geo.cpp
@@ -5,12 +5,14 @@
 
 namespace geo {
 
+constexpr double EARTH_RADIUS = 6371000.0;
+
 double ComputeDistance(Coordinates from, Coordinates to) {
     using namespace std;
     const double dr = M_PI / 180.0;
-    return acos(sin(from.lat * dr) * sin(to.lat * dr)
-                + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * 6371000;
+    return acos(sin(from.lat * dr) * sin(to.lat * dr) +
+                cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
+        * EARTH_RADIUS;
 }
 
 }  // namespace geo
